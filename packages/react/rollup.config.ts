4e86142a58849676642a8a681fc3a85191fd6d62
Lorenzo <lorenzobloedow@gmail.com>
2023-09-16 13:14:01 -0300

    Add rollup plugin to preserve 'use client' directives

--- packages/react/rollup.config.ts
+++ packages/react/rollup.config.ts
@@ -1,8 +1,9 @@
+import preserveDirectives from "rollup-plugin-preserve-directives";
 import { mergeDefaultRollupConfig } from "dev-utils";
 
 export default mergeDefaultRollupConfig({
 	newConfig: {
-		plugins: { typescript: { tsconfig: "./tsconfig.json" } },
+		plugins: { typescript: { tsconfig: "./tsconfig.json" }, extra: [preserveDirectives()] },
 		root: {
 			input: ["src/browser/index.ts", "src/hybrid/index.ts", "src/node/index.ts"],
 			output: {
